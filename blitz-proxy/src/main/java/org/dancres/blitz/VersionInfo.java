65d09f78d48335346379873a25ddc126e955c955
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2014-03-14 11:39:49 +0100

    remove dependency on je from blitz-proxy; update dependency on rio to M4-S6

--- blitz-proxy/src/main/java/org/dancres/blitz/VersionInfo.java
+++ blitz-proxy/src/main/java/org/dancres/blitz/VersionInfo.java
@@ -1,9 +1,11 @@
 package org.dancres.blitz;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.sleepycat.je.JEVersion;
 import org.dancres.blitz.stats.Stat;
 import org.dancres.blitz.stats.StatGenerator;
 import org.dancres.blitz.stats.StatsBoard;
@@ -22,10 +24,28 @@ public class VersionInfo {
     public static final String PRODUCT_NAME = "Blitz JavaSpaces (PureJavaEdition)";
     public static final String EMAIL_CONTACT = "info@sorcersoft.com";
     public static final String SUPPLIER_NAME = "SorcerSoft.com S.A.";
-    public static final String VERSION = "2.3";
+    public static final String VERSION;
+    public static final String VERSION_JE;
 
     static {
         StatsBoard.get().add(new Generator());
+        Properties props = new Properties();
+        InputStream versions = null;
+        try {
+            versions = VersionInfo.class.getResourceAsStream("VersionInfo.properties");
+            props.load(versions);
+            VERSION=props.getProperty("blitz");
+            VERSION_JE=props.getProperty("sleepycat");
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        } finally {
+            if (versions != null)
+                try {
+                    versions.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 
     private static class Generator implements StatGenerator {
@@ -47,8 +67,7 @@ public class VersionInfo {
 
     private static String versionString() {
         return PRODUCT_NAME + ", " + EMAIL_CONTACT + ", " +
-                SUPPLIER_NAME + ", " + VERSION + ", Db/Java " +
-                JEVersion.CURRENT_VERSION.getVersionString();
+                SUPPLIER_NAME + ", " + VERSION + ", Db/Java ${sleepycat.version}";
     }
 
     public static void dump() {
